237ff08e410a1aceeeb27f53aa1a0be1e18f5b0b
Farhan-Zafar-Aziz <139948637+Farhan-Zafar-Aziz@users.noreply.github.com>
2023-08-21 21:40:15 +0500

    Typescript Fundamentals

---
+++ Q-01 PIAIC/Fri.ts
@@ -0,0 +1,124 @@
+//var firstName = "Ali" 
+//console.log(firstName);
+
+//var pak = "Hello Pakistan";
+//console.log(pak);
+
+//var PIAIC = "Presidential Initiative for Artificial Intelligence and Computing";
+//console.log(PIAIC);
+
+//var PAF = "Pakistan Air Force";
+//console.log(PAF);
+
+//var myMessage = "Hello Sir";
+//console.log(myMessage);
+
+//let firstName = "Al Aziz";
+//console.log(firstName);
+
+//let nbp = "National Bank of Pakistan";
+//console.log(nbp);
+
+//const imf = "International Mutual Funds";
+//console.log(imf);
+
+//const fzAr = "Farhan Zafar Aziz Rajar";
+//console.log(fzAr);
+
+//let myName = "Alhamdulillah";
+//let message = 123;
+//let aziz = true;
+
+//let firstName;
+//console.log(firstName)
+
+//let empty = null;
+
+//let firstName = "Sadiq Iqbal Rajar.";
+//let study = "Quaid-e-Awam University";
+//let department = "Computer Science";
+//let message = `Hello Dear ${firstName} I hope you received this message from me.When I heared this, You cleared the your entry test I'm so happy and want to hugg you and celebrate at the time, but I can't to come for this hearted venue due to some issues and I appreciate you and Congratulations Dear on your this success May Allah give you long live and successes.Now I am saying appreciatly that My Brother selected in ${study} in ${department} world which is the King of "Modern World".`;
+//console.log(message)
+
+//let firstName = "Farhan Aziz";
+//let age = 19;
+//let message2 = `And my name is ${firstName} and my age is ${age}`;
+//console.log(message2)
+
+//let fathers = "Zafar Aziz";
+//let age2="Mamoo";
+//let message = `My fathers name is ${fathers} and he is ${age2} of ${firstName1}`;
+//console.log(message);
+
+//let a = 2
+//let b = 4
+//let c = a+b
+//console.log(c) ;
+
+//let b = 4
+//let a = 2
+//let c = b-a
+//console.log(c);
+
+//let a = 6
+//let b = 2
+//let c = a/b
+//console.log(c)
+
+//let a = 2
+//let b = 6
+//let c = 2*6
+//console.log(c)
+
+//let a = 5
+//let b = 6
+//let c = 5**6
+//console.log(c)
+
+//let isMarried = false;
+//console.log(typeof isMarried)
+
+//let string = "Hello Dunya"
+//console.log(typeof string)
+
+//let number = 123;
+//console.log(typeof number)
+
+//let totalMarks = 550;
+//let obtainMarks = 451;
+//let result = totalMarks%obtainMarks 
+//console.log(result)
+
+//let a = 10;
+//let b = + 5;
+//console.log (a);
+//a - 5;
+//console.log(a)
+//a * 5;
+//console.log(a)
+
+//let num1 = 8
+//let num2 = 4
+//let result = (num1/num2)
+//console.log(result)
+//console.log(num1 + num2)
+//console.log(num2 - num1)
+//console.log(num1 / num2)
+//console.log(num1 * num2)
+
+//let firstWord = "F"
+//let secondWord = "A"
+//let thiredWord = "R"
+//let fourthWord = "H"
+//let fifthWord = "A"
+//let sixthWord = "N"
+ 
+//let myfullName =  `${firstWord}${secondWord}${thiredWord}${fourthWord}${fifthWord}${sixthWord}`
+//console.log(myfullName)
+        
+
+    
+
+
+
+
